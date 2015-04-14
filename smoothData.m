db4175f5183111d0a660a6f10c466dddb7dbaf5c
James Taylor <jamestaylr@gmail.com>
2015-04-14 14:41:37 -0400

    Created data smoothing function
    The `smoothData()` function creates a matrix with a given number of
    rows. It averages all values across and pairs them with a complementary
    vector.

---
+++ smoothData.m
@@ -0,0 +1,26 @@
+function [COMPLEMENT, SMOOTHED] = smoothData(COMPLEMENT, RAW, SMOOTHING_CONSTANT)
+
+	% Seperates the data into a matrix, with SMOOTHING_CONSTANT columns
+	J = [];
+	for i=1:1:SMOOTHING_CONSTANT
+		J = horzcat(J, RAW(i:SMOOTHING_CONSTANT:end-mod(length(RAW), SMOOTHING_CONSTANT)));
+	end
+
+	SMOOTHED = [];
+
+	% Iterates over the rows of the matrix and takes the average of each row
+	for j=1:1:size(J)[1];
+		try
+
+			SMOOTHED = [SMOOTHED; mean(J(j, :))];
+
+		catch ME
+			% Throws an error exception if the data cannot be averages
+			disp('The data is malformed! It cannot be averaged!');
+			return;
+		end
+	end
+
+	% Takes every SMOOTHING_CONSTANT value to make a complement to the smoothed data
+	COMPLEMENT = COMPLEMENT(1:SMOOTHING_CONSTANT:end-mod(length(COMPLEMENT), SMOOTHING_CONSTANT));
+
