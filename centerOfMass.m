a9e31e90634c9487200a8eb4dfb666eecc6b2033
James Taylor <jamestaylr@gmail.com>
2015-04-23 23:16:08 -0400

    Added comments and formatting changes

--- centerOfMass.m
+++ centerOfMass.m
@@ -1,13 +1,21 @@
 function [CMASS] = centerOfMass(COMPLEMENT, DATA)
 
+% Set the initial values
 NUMSUM = 0;
 DENOSUM = 0;
 
-for i = 1:1:numel(t);
-   NUMSUM = COMPLEMENT(i) * DATA(i) + NUMSUM;
-   DENOSUM = DATA(i) + DENOSUM;
+% Iterates through all the time values
+for i = 1:1:numel(COMPLEMENT);
+	% Calculates the center of mass
+	NUMSUM = (COMPLEMENT(i) * DATA(i)) + NUMSUM;
+	DENOSUM = DATA(i) + DENOSUM;
 
 end
 
+% Return the center of mass
 CMASS = NUMSUM / DENOSUM;
 
+%{
+Sample usage:
+CMASS = centerOfMass(DATA(:,1), DATA(:,2));
+%}
