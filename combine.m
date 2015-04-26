3fc6b5e3b81da58663b2074b544a05a9246df9ac
James Taylor <jamestaylr@gmail.com>
2015-04-26 17:08:05 -0400

    Created method to combine disjoint data sets
    The `combine()` method takes a variable number of arguments and returns
    a new matrix with the combined rows of all the input matrices.

---
+++ combine.m
@@ -0,0 +1,29 @@
+function [COMBINED] = combine(varargin)
+
+	% Extracts the dimenions of the first cell
+	[m n] = size(varargin{1});
+    B = [];
+   
+    % Iterating through each of the columns
+    for i = 1:1:n
+        % For each column, go through all the data sets in the variable
+        % arguments
+        S = varargin{1}(:,i);
+        for d = 2:1:length(varargin)
+            S = vertcat(S, varargin{d}(:,i));
+        end
+        
+        B = horzcat(B, S);
+    end
+    
+    COMBINED = B;
+
+%{
+Sample usage:
+Data1 = readFile('data-1');
+Data2 = readFile('data-2');
+DATA = combine(Data1, Data1);
+
+Note: This function takes a variable number of arguments
+%}
+
