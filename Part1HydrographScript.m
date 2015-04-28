ab5ab43aae7b2fd36441eda2c778b87adeccbcd3
Autumn Diehl <autumndiehl15@gmail.com>
2015-04-28 00:20:38 -0400

    Changed Part 1 Hydrograph
    It now outputs a graph, but now I need to go back and match the
    corresponding storm duration to the flow duration

--- Part1HydrographScript.m
+++ Part1HydrographScript.m
@@ -3,5 +3,6 @@
 DATA4 = readFile('sample-data/flow-data/data-1.csv');
 DATA5 = readFile('sample-data/flow-data/data-2.csv');
 DATA6 = readFile('sample-data/flow-data/data-3.csv');
-FLOW_DATA = truncateData(combine(DATA4, DATA5, DATA6));
-plot(FLOW_DATA(:,1),FLOW_DATA(:,2))
+FLOW_DATA = combine(DATA4, DATA5, DATA6);
+TRUNCATED = truncateData(FLOW_DATA(:,2), 0, max(FLOW_DATA(:, 2)));
+plot(FLOW_DATA(:,1), TRUNCATED);
