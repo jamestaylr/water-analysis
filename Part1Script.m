b40d8e038cf113fbe493423ab540483633c70b15
James Taylor <jamestaylr@gmail.com>
2015-04-27 22:58:30 -0400

    Miscellaneous formatting changes

--- Part1Script.m
+++ Part1Script.m
@@ -6,26 +6,26 @@ DATA3 = readFile('sample-data/precipitation-data/data-3.csv');
 % Combine all the data sets
 DATA = combine(DATA1, DATA2, DATA3);
 
-%Converts rainfall data to intensity
+% Converts rainfall data to intensity
 [INTENSITY, DATA] = calculateIntensity (DATA(:,1), DATA(:,2));
 
-%Calculates duration of longest storm-NOT DONE. TROY IS WORKING ON LOOP
+% Calculates duration of longest storm-NOT DONE. TROY IS WORKING ON LOOP
 [EVENT, DURATION] = stormDuration (DATA);
 disp(DURATION)
 
-%Hyetograph plot
+% Hyetograph plot
 TIME = DATA(0:5:end); %5 minute time intervals
 plot(INTENSITY, TIME)
 xlabel('Time[min]')
 ylabel('Intensity[in/hr]')
 title('Hyetograph: Rainfall Intensity')
-%Should be able to add hydrograph to hyetograph using hold on
+% Should be able to add hydrograph to hyetograph using hold on
 
-%Calculates peak intensity in duration of storm
-PEAK_INTENSITY = max(DATA(:,1)); %Should this be 2 or 1 for the peak intensity?
-VOLUME = %Need to output the volume of the rainfall during the duration of the storm
+% Calculates peak intensity in duration of storm
+PEAK_INTENSITY = max(DATA(:,1)); % Should this be 2 or 1 for the peak intensity?
+VOLUME = % Need to output the volume of the rainfall during the duration of the storm
 fprintf('\nThe peak intensity calculated from the rainfall data is %0.2f\n',PEAK_INTENSITY);
 fprintf('\nThe volume calculated from the rainfall during the duration of the storm is %0.2f\n', VOLUME);
 
-%Begin working with stormwater runoff data and hydrograph
+% Begin working with stormwater runoff data and hydrograph
 
--- stormDuration.m
+++ stormDuration.m
@@ -1,11 +1,10 @@
 function [EVENT,DURATION] = stormDuration (RAINFALL_DATA)
-%Event is the storm. DATA(:,1) is the first column, time, and
-%DATA(:,2) is the rainfall data in the second column
+
 EVENT = RAINFALL_DATA > 0;
 
 EVENT = [0 EVENT 0];
 
+% Before the event before the storm ends
 BEFORE = find(diff(EVENT));
-%Before is the event before the storm ends
 
 DURATION = BEFORE(2:2:end) - BEFORE(1:2:end);
