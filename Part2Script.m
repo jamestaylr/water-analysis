3ad3f5b29fcc81062bd5affa0e2bc342ab6cf89f
jamestaylr <jamestaylr@gmail.com>
2015-04-29 01:03:19 -0400

    Added repeating while loop for Part 2 script

--- Part2Script.m
+++ Part2Script.m
@@ -1,11 +1,27 @@
-PART = input('Which water quantity date parameter would you like to test?/n(1 = Chlorine Concentration, 2 = PH, 3 = Turbidity): ');
-if PART == 1;
-    run Part2ChlorineScript
-elseif PART == 2;
-    run Part2PHScript
-elseif PART == 3;
-    run Part2TurbidityScript
-else
-    display('The entered value is incorrect!');
-    PART = input('Which water quantity date parameter would you like to test?/n(1 = Chlorine Concentration, 2 = PH, 3 = Turbidity): ');
+% Repeat the loop until an acceptable number is entered
+flag = false
+
+while ~flag
+	% Prompt the user for the data parameter to display
+	PART = input('Which water quantity parameter would you like to test?/n(1 = Chlorine Concentration, 2 = PH, 3 = Turbidity): ');
+	
+	if PART == 1
+		flag = true;
+	    run Part2ChlorineScript;
+	    break;
+
+	elseif PART == 2
+		flag = true;
+	    run Part2PHScript;
+	    break;
+
+	elseif PART == 3
+		flag = true;
+	    run Part2TurbidityScript;
+	    break;
+
+	else
+	    display('The entered value is incorrect! Please try again!');
+	end
+
 end
