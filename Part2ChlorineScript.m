88baffbbd9f919884f81fcb0d1fdc2eaadf99426
ronburg <troybe1@vt.edu>
2015-04-28 23:05:20 -0400

    Fixed chlorine data conversion error

--- Part2ChlorineScript.m
+++ Part2ChlorineScript.m
@@ -5,10 +5,10 @@ clf
 CHLORINE_DATA = readFile('sample-data/water-quality-data/data.csv');
 
 THRESHOLD = input('Type a threshold value: ');
-CHLORINE_DATA(:,2) = .33.*CHLORINE_DATA(:,7);
+CONVERTED = .33.*CHLORINE_DATA(:,7);
 
 % Plot the chlorine data
-plot(CHLORINE_DATA(:,1), CHLORINE_DATA(:,7));
+plot(CHLORINE_DATA(:,1), CONVERTED);
 hold on;
 
 % Create linear space to act as the constant threshold
@@ -18,13 +18,13 @@ j = linspace(THRESHOLD, THRESHOLD, length(CHLORINE_DATA(:,1)));
 plot(CHLORINE_DATA(:,1), j(:), 'r');
 
 % Find all the regions that the threshold is exceeded
-RESULTS = findMultipleEvents(CHLORINE_DATA(:, 7), THRESHOLD, true);
+RESULTS = findMultipleEvents(CONVERTED, THRESHOLD, true);
 
 [m n] = size(RESULTS);
 
 % Loop through all the rows of the response matrix
 for i = 1:1:m
 	fprintf('For region %d:\n', i)
-	fprintf('The peak chlorine concentration is %0.4f\n', max(CHLORINE_DATA(RESULTS(i,1):RESULTS(i,2),7)));
+	fprintf('The peak chlorine concentration is %0.4f\n', max(CONVERTED(RESULTS(i,1):RESULTS(i,2))));
 	fprintf('Duration of the event: %d minutes\n\n', (RESULTS(i,2) - RESULTS(i,1)) * 5);
 end
