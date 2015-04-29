291d9c98961c7f482a27b78e626e823d7fdeb3de
Autumn Diehl <autumndiehl15@gmail.com>
2015-04-29 01:46:48 -0400

    Implemented flow data smoothing for Part 1 hydrograph script

--- Part1HydrographScript.m
+++ Part1HydrographScript.m
@@ -1,10 +1,13 @@
-% Part 1 Hydrograph
-% Begin working with stormwater runoff data and hydrograph
+% Clear the figure window
+clf;
+
 FLOW_DATA = input('Please input the flow data you would like to run: ','s');
 FLOW_DATA = readFile(FLOW_DATA);
 FLOW_DATA(:,2) = truncateData(FLOW_DATA(:,2), 0, max(FLOW_DATA(:, 2)));
-[START, STOP, DURATION] = findLongestDuration(FLOW_DATA(:,2), 0, 0.0001);
 
-%Smooth duration graph
-[FLOW_DATA(:,1), FLOW_DATA(:,2)] = smoothData(FLOW_DATA(START:STOP,1), FLOW_DATA(START:STOP,2), 20);
-plot(FLOW_DATA(:,1),FLOW_DATA(:,2))
+% Smooth flow data graph
+[X, Y] = smoothData(FLOW_DATA(:,1), FLOW_DATA(:,2), 40);
+plot(X, Y);
+title('Hydrograph: Flow Rate')
+xlabel('Time[sec]')
+ylabel('Flow Rate[m^3/s]')
