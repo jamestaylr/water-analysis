c02a1c96babd0c8268d57de0832241a62639cb4b
Autumn Diehl <autumndiehl15@gmail.com>
2015-04-29 00:45:21 -0400

    Implemented the Part 1 hydrograph script

--- Part1HydrographScript.m
+++ Part1HydrographScript.m
@@ -1,8 +1,8 @@
-% Part 1 Hydrograph
-% Begin working with stormwater runoff data and hydrograph
+% Read in the flow data
 DATA4 = readFile('sample-data/flow-data/data-1.csv');
 DATA5 = readFile('sample-data/flow-data/data-2.csv');
 DATA6 = readFile('sample-data/flow-data/data-3.csv');
 FLOW_DATA = combine(DATA4, DATA5, DATA6);
 TRUNCATED = truncateData(FLOW_DATA(:,2), 0, max(FLOW_DATA(:, 2)));
+
 plot(FLOW_DATA(:,1), TRUNCATED);
