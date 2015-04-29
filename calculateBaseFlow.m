e7c99b259a59c9b0380e3d219c6a34848ddb863c
jamestaylr <jamestaylr@gmail.com>
2015-04-29 17:42:07 -0400

    Now using `mode()` in base flow calculations

--- calculateBaseFlow.m
+++ calculateBaseFlow.m
@@ -1,9 +1,9 @@
 function [BASE_FLOW] = calculateBaseFlow(DATA)
 
-	MEDIAN = median(DATA);
+	MODE = mode(DATA);
 	STD = std(DATA);
 
-	DATA = DATA(DATA<MEDIAN+STD);
-	DATA = DATA(DATA>MEDIAN-STD);
+	DATA = DATA(DATA < MODE + STD);
+	DATA = DATA(DATA > MODE - STD);
 
 	BASE_FLOW = mean(DATA);
