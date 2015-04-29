bd00be136d5b2e16a72beac1b826bc91c7c8a5b8
Autumn Diehl <autumndiehl15@gmail.com>
2015-04-29 18:39:54 -0400

    Added to Hyetograph

--- Part1HyetographScript.m
+++ Part1HyetographScript.m
@@ -11,19 +11,17 @@ INTENSITY = truncateData(INTENSITY, 0, max(INTENSITY));
 X = PRECIPITATION_DATA(:,1);
 plot(X, INTENSITY)
 bar(X, INTENSITY)
-
-% axis([0 PRECIPITATION_DATA(:,1) 0 max(INTENSITY)]) 
-% [START, STOP, DURATION] = findLongestDuration(PRECIPITATION_DATA(:,2), 0, 0.00001);
+formatTime(X); 
 
 % bar(PRECIPITATION_DATA(START:STOP,1), PRECIPITATION_DATA(START:STOP,2));
-% xlabel('Time[min]');
-% ylabel('Intensity[in/hr]');
-% title('Hyetograph: Rainfall Intensity');
+xlabel('Time[min]');
+ylabel('Intensity[in/hr]');
+title('Hyetograph: Rainfall Intensity');
 
-% PEAK_INTENSITY = max(PRECIPITATION_DATA(START:STOP,2));
-% VOLUME = ((sum(PRECIPITATION_DATA(:,2))*5)/60); % James is creating a function for Volume
-% COM = centerOfMass(PRECIPITATION_DATA(:,1), PRECIPITATION_DATA(:,2));
-% fprintf('The duration of the longest rainstorm event is %0.2f minutes.\n',DURATION);
-% fprintf('The peak intensity calculated from the rainfall data is %0.2f inches per hour.\n', PEAK_INTENSITY);
-% fprintf('The volume calculated from the rainfall during the duration of the storm is %0.2f inches cubed.\n', VOLUME);
-% fprintf('The x coordinate of the center of mass is %0.2f.\n',COM);
+PEAK_INTENSITY = max(INTENSITY);
+%VOLUME = sum(INTENSITY)
+COM = centerOfMass(PRECIPITATION_DATA(:,1), PRECIPITATION_DATA(:,2));
+%fprintf('The duration of the longest rainstorm event is %0.2f minutes.\n',DURATION);
+fprintf('The peak intensity calculated from the rainfall data is %0.2f inches per hour.\n', PEAK_INTENSITY);
+%fprintf('The volume calculated from the rainfall during the duration of the storm is %0.2f inches cubed.\n', VOLUME);
+fprintf('The x coordinate of the center of mass is %0.2f.\n',COM);
