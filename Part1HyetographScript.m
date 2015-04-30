3851367bf5da00885e88a57190e98486dbd6a18b
James Taylor <jamestaylr@gmail.com>
2015-04-29 23:14:05 -0400

    Finished hyetograph script

--- Part1HyetographScript.m
+++ Part1HyetographScript.m
@@ -4,24 +4,25 @@ clf
 % Prompt the user for the rainfall data
 DATA = input('Please input the rainfall data you would like to analyze:','s');
 
+AREA = input('Please input the area of the watershed in km^2 (Webb Branch is 2.78km^2):');
+
 PRECIPITATION_DATA = readFile(DATA);
 INTENSITY = PRECIPITATION_DATA(:,2).*60;
 INTENSITY = truncateData(INTENSITY, 0, max(INTENSITY));
 
 X = PRECIPITATION_DATA(:,1);
-plot(X, INTENSITY)
-bar(X, INTENSITY)
-formatTime(X); 
 
-% bar(PRECIPITATION_DATA(START:STOP,1), PRECIPITATION_DATA(START:STOP,2));
+bar(X, INTENSITY, 20);
+formatTime(X); 
 xlabel('Time[min]');
 ylabel('Intensity[in/hr]');
 title('Hyetograph: Rainfall Intensity');
 
 PEAK_INTENSITY = max(INTENSITY);
-%VOLUME = sum(INTENSITY)
+VOLUME = (sum(INTENSITY.*.254*AREA*100000)/60);
 COM = centerOfMass(PRECIPITATION_DATA(:,1), PRECIPITATION_DATA(:,2));
-%fprintf('The duration of the longest rainstorm event is %0.2f minutes.\n',DURATION);
+DURATION = calculateDuration(INTENSITY);
+fprintf('The duration of the rainstorm event is %0.2f minutes.\n',DURATION);
 fprintf('The peak intensity calculated from the rainfall data is %0.2f inches per hour.\n', PEAK_INTENSITY);
-%fprintf('The volume calculated from the rainfall during the duration of the storm is %0.2f inches cubed.\n', VOLUME);
+fprintf('The volume calculated from the rainfall during the duration of the storm is %0.2f meters cubed.\n', VOLUME);
 fprintf('The x coordinate of the center of mass is %0.2f.\n',COM);
