b654f81d07368686c9be8acf0250f175ce5e88eb
James Taylor <jamestaylr@gmail.com>
2015-04-30 01:13:37 -0400

    Added lag time calculation to Part 1 script

--- Part1Script.m
+++ Part1Script.m
@@ -1,3 +1,5 @@
 % Runs all the Part 1 scripts, generating both a hydrograph and hyetograph
 run Part1HydrographScript
 run Part1HyetographScript
+
+fprintf('\nLag time: %0.2f [hours]\n', (PEAK_FLOW-COM)*24);
