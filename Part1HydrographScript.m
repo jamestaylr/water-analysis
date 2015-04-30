377d17fb21e71519bbfc8e7a9c50017441c27aa4
James Taylor <jamestaylr@gmail.com>
2015-04-30 01:14:49 -0400

    Added peak flow calculation to Part 1 hydrograph script

--- Part1HydrographScript.m
+++ Part1HydrographScript.m
@@ -1,7 +1,7 @@
 % Clear the figure window
 clf;
 
-FLOW_DATA = input('Please input the flow data you would like to run: ','s');
+FLOW_DATA = input('Please input the flow data you would like to analyze: ','s');
 FLOW_DATA = readFile(FLOW_DATA);
 FLOW_DATA(:,2) = truncateData(FLOW_DATA(:,2), 0, max(FLOW_DATA(:, 2)));
 
@@ -12,11 +12,14 @@ BASE_FLOW = calculateBaseFlow(FLOW_DATA(:,2));
 [X, Y] = smoothData(FLOW_DATA(:,1), FLOW_DATA(:,2), 40);
 hold on
 plot(X, Y, '-', X, BASE_FLOW, ':');
-formatTime(X);
+% formatTime(X);
 hold off
 title('Hydrograph: Flow Rate')
-xlabel('Time[sec]')
-ylabel('Flow Rate[m^3/s]')
+xlabel('Time [minute]')
+ylabel('Flow Rate [m^3/s]')
 axis tight
 legend('Flow Rate','Baseflow');
-fprintf('The baseflow of the data is %0.4f\n',BASE_FLOW);
+fprintf('The baseflow of the data is %0.4f\n', BASE_FLOW);
+[m i] = max(FLOW_DATA(:,2));
+PEAK_FLOW = FLOW_DATA(i:i, 1);
+fprintf('The peak flow of the data is %0.4f\n', PEAK_FLOW);
