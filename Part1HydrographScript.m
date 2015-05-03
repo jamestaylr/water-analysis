6e3cefb465ad52df953dc60205cdab1e225adc48
James Taylor <jamestaylr@gmail.com>
2015-05-03 16:16:48 -0400

    Minor code formatting changes

--- Part1HydrographScript.m
+++ Part1HydrographScript.m
@@ -20,7 +20,7 @@ ylabel('Flow Rate [m^3/s]')
 axis tight
 legend('Flow Rate','Baseflow');
 
-VOLUME = (sum(FLOW_DATA(:,2))*30) - (BASE_FLOW * length(FLOW_DATA(:,1)));
+VOLUME = (sum(FLOW_DATA(:,2)) * 30) - (BASE_FLOW * length(FLOW_DATA(:,1)));
 [m, i] = max(FLOW_DATA(:,2));
 
 [DURATION, FLOW_START, FLOW_END] = calculateDuration(FLOW_DATA(:,2));
@@ -28,4 +28,4 @@ VOLUME = (sum(FLOW_DATA(:,2))*30) - (BASE_FLOW * length(FLOW_DATA(:,1)));
 fprintf('The baseflow of the data is %0.4f\n', BASE_FLOW);
 fprintf('The peak flow of the data is %0.4f\n', m);
 fprintf('The volume of the hydrograph omitting the baseflow: %0.2f [meters^3]\n', VOLUME);
-fprintf('The flow event started at %0.2f and ended at %0.2f, with a peak flow at time %0.2f.', FLOW_START, FLOW_END, FLOW_DATA(i:i, 1));
+fprintf('The flow event started at %0.2f and ended at %0.2f, with a peak flow at time %0.2f.\n', FLOW_START, FLOW_END, FLOW_DATA(i:i, 1));
