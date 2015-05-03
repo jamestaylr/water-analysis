ad7e4bd474a21e6214662f3689924c78c1624b60
jamestaylr <jamestaylr@gmail.com>
2015-05-03 15:28:28 -0400

    Miscellaneous logic fixes to Part 1 hyetograph script

--- Part1HyetographScript.m
+++ Part1HyetographScript.m
@@ -3,27 +3,24 @@ clf
 
 % Prompt the user for the rainfall data
 DATA = input('Please input the rainfall data you would like to analyze: ','s');
-
-AREA = input('Please input the area of the watershed in km^2 (Webb Branch is 2.78km^2): ');
+AREA = input('Please input the area of the watershed in km^2 (Webb Branch is 2.78km^2):');
 
 PRECIPITATION_DATA = readFile(DATA);
-INTENSITY = PRECIPITATION_DATA(:,2).*60;
+INTENSITY = PRECIPITATION_DATA(:,2) .* 60;
 INTENSITY = truncateData(INTENSITY, 0, max(INTENSITY));
 
-[DURATION, START, STOP] = calculateDuration(INTENSITY);
+X = PRECIPITATION_DATA(:,1);
 
-[X, Y] = smoothData(PRECIPITATION_DATA(START:STOP,1), PRECIPITATION_DATA(START:STOP,2), 20);
-
-bar(X, Y);
-% formatTime(X); 
-
-xlabel('Time [min]');
-ylabel('Intensity [in/hr]');
+bar(X, INTENSITY, 20);
+formatTime(X); 
+xlabel('Time[min]');
+ylabel('Intensity[in/hr]');
 title('Hyetograph: Rainfall Intensity');
 
 PEAK_INTENSITY = max(INTENSITY);
-VOLUME = (sum(INTENSITY)*(1/12)*(0.0254)*(DURATION)*(AREA));
+VOLUME = (sum(INTENSITY .* 0.254 * AREA * 100000) / 60);
 COM = centerOfMass(PRECIPITATION_DATA(:,1), PRECIPITATION_DATA(:,2));
+DURATION = calculateDuration(INTENSITY);
 
 fprintf('The duration of the rainstorm event is %0.2f minutes.\n', DURATION);
 fprintf('The peak intensity calculated from the rainfall data is %0.2f inches per hour.\n', PEAK_INTENSITY);
