51532c888d4b3ba6c2e16f78c4c894ab9257f9ca
Autumn Diehl <autumndiehl15@gmail.com>
2015-05-03 18:18:49 -0400

    Changed output of start and stop indices
    The dates of the events reported in the output statements are formatted correctly.

--- Part1HyetographScript.m
+++ Part1HyetographScript.m
@@ -3,7 +3,7 @@ clf;
 
 % Prompt the user for the rainfall data
 PRECIPITATION_DATA_NAME = input('Please input the rainfall data you would like to analyze: ','s');
-AREA = input('Please input the area of the watershed in km^2 (Webb Branch is 2.78km^2):');
+AREA = input('Please input the area of the watershed in km^2 (Webb Branch is 2.78km^2): ');
 
 PRECIPITATION_DATA = readFile(PRECIPITATION_DATA_NAME);
 INTENSITY = PRECIPITATION_DATA(:,2) .* 60;
@@ -20,4 +20,4 @@ title('Hyetograph: Rainfall Intensity');
 PEAK_INTENSITY = max(INTENSITY);
 VOLUME_HYETOGRAPH = (sum(INTENSITY .* 0.254 * AREA * 100000) / 60);
 MASS_CENTER = centerOfMass(PRECIPITATION_DATA(:,1), PRECIPITATION_DATA(:,2));
-DURATION_HYETOGRAPH = calculateDuration(INTENSITY);
+[DURATION_HYETOGRAPH, HYETOGRAPH_START, HYETOGRAPH_END] = calculateDuration(INTENSITY);
