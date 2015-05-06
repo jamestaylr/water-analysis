e780c926e1be62144050e69704cff9433b243cf8
Autumn Diehl <autumndiehl15@gmail.com>
2015-05-05 20:51:52 -0400

    Minor output changes
    Minor spelling errors have been fixed.

--- Part2ChlorideScript.m
+++ Part2ChlorideScript.m
@@ -8,13 +8,14 @@ CHLORIDE_DATA = readFile(CHLORIDE_DATA_NAME);
 THRESHOLD = input('Type a threshold value: ');
 CONVERTED = .33.*CHLORIDE_DATA(:,7);
 
-% Plot the chlorine data
+% Plot the chloride data
 plot(CHLORIDE_DATA(:,1), CONVERTED);
 xlabel('Time')
-ylabel('Specific Conductivity (mg/L)')
-title('Chloride Concentration Plot')
+ylabel('Specific Conductivity (microSiemens/cm)')
+title('Specific Conductivity Events ')
 hold on;
 formatTime(CHLORIDE_DATA(:,1));
+
 % Create linear space to act as the constant threshold
 j = linspace(THRESHOLD, THRESHOLD, length(CHLORIDE_DATA(:,1)));
 
@@ -26,6 +27,6 @@ RESULTS = findMultipleEvents(CONVERTED, THRESHOLD, true);
 
 [m, n] = size(RESULTS);
 
-fprintf('For the given data:')
+fprintf('For the given data:\n')
 fprintf('The peak chloride concentration is %0.2f\n', max(CONVERTED(RESULTS(:,1):RESULTS(:,2))));
-fprintf('Duration of the event: %d minutes\n\n', (RESULTS(:,2) - RESULTS(:,1)) * 5);
+fprintf('Total time Specific Conductivity was greater than the threshold: %d minutes\n\n', (RESULTS(:,2) - RESULTS(:,1)) * 5);
