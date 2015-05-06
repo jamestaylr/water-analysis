99a5209beeb4daed9bf3acdaa0b8ec3698fa12c5
James Taylor <jamestaylr@gmail.com>
2015-05-05 20:52:04 -0400

    Removed Boolean flags from scripts
    When the user enters an acceptable value, a modular script runs and the
    loop breaks. The Boolean flag to escape the while loop is unnecessary.

--- Part2Script.m
+++ Part2Script.m
@@ -1,22 +1,17 @@
 % Repeat the loop until an acceptable number is entered
-flag = false;
-
-while ~flag
+while true
 	% Prompt the user for the data parameter to display
 	PART = input('Which water quantity parameter would you like to test? (1 = Chloride Concentration, 2 = PH, 3 = Turbidity): ');
 	
 	if PART == 1
-		flag = true;
 	    run Part2ChlorideScript;
 	    break;
 
 	elseif PART == 2
-		flag = true;
 	    run Part2PHScript;
 	    break;
 
 	elseif PART == 3
-		flag = true;
 	    run Part2TurbidityScript;
 	    break;
 
--- Script.m
+++ Script.m
@@ -1,17 +1,13 @@
 % Repeat the loop until an acceptable number is entered
-flag = false;
-
-while ~flag
+while true
 	% Prompt the user for the data parameter to display
 	PART = input('Would you like to test water quality or quantity? (1 = Water Quality, 2 = Water Quantity): ');
 	
 	if PART == 1
-		flag = true;
 	    run Part1Script;
 	    break;
 
 	elseif PART == 2
-		flag = true;
 	    run Part2Script;
 	    break;
 
